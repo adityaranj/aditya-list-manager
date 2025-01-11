70e67aa6d652c35e588ea19b2b65ec14c9a46709
Aditya Ranjan <90023096+adityaranj@users.noreply.github.com>
2025-01-11 14:10:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ItemListManager from './ItemListManager'; // Import the component
+
+function App() {
+  return (
+    <div className="App">
+      <ItemListManager />
+    </div>
+  );
+}
+
+export default App;
